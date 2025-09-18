6c1d506c29c336b7b9952366bcd22a13c62c28f7
Shawn Daichendt <shawn.daichendt@gmail.com>
2025-09-18 13:59:38 -0400

    Refactor quest creation handler to streamline PrismaClient and questQueue usage

--- apps/nuxt/server/api/quests/index.post.ts
+++ apps/nuxt/server/api/quests/index.post.ts
@@ -1,19 +1,18 @@
 import { PrismaClient } from '@prisma/client'
 
-const prisma = new PrismaClient()
-
 export default defineEventHandler(async (event) => {
-  const questQueue = useNuxtApp().$questQueue
-
-  // read body
   const body = await readBody(event)
   const { title, description, userId } = body
+
+  const prisma = new PrismaClient()
+
+  const { $questQueue } = useNuxtApp()
 
   const quest = await prisma.quest.create({
     data: { title, description, ownerId: userId }
   })
 
-  await questQueue.add('decompose', { questId: quest.id, title, description })
+  await $questQueue.add('decompose', { questId: quest.id, title, description })
 
   return { success: true, quest }
 })
