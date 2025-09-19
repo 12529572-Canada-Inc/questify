24a1d62863096f844c3f58616937b0f2282607e3
Shawn Daichendt <shawn.daichendt@gmail.com>
2025-09-19 10:28:32 -0400

    Refactor code structure for improved readability and maintainability

--- apps/nuxt/nuxt.config.ts
+++ apps/nuxt/nuxt.config.ts
@@ -1,12 +1,13 @@
 export default defineNuxtConfig({
   nitro: {
-    compatibilityDate: '2025-09-17', // update this every 3 months
+    compatibilityDate: "2025-09-17", // update this every 3 months
   },
+  modules: ["@nuxt/eslint"],
   runtimeConfig: {
     redis: {
-      host: process.env.REDIS_HOST || 'localhost',
-      port: process.env.REDIS_PORT || '6379',
-      password: process.env.REDIS_PASSWORD || ''
-    }
+      host: process.env.REDIS_HOST || "localhost",
+      port: process.env.REDIS_PORT || "6379",
+      password: process.env.REDIS_PASSWORD || "",
+    },
   },
-})
+});
