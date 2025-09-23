d45ee5f23b3276cf91cef329f49e50b9e3a11942
Shawn Daichendt <shawn.daichendt@gmail.com>
2025-09-23 19:45:23 -0400

    refactor: add parseRedisUrl utility and update Redis configuration in Nuxt app

--- apps/nuxt/server/plugins/quest-queue.server.ts
+++ apps/nuxt/server/plugins/quest-queue.server.ts
@@ -1,15 +1,17 @@
 import { Queue } from 'bullmq'
+import { parseRedisUrl } from '~/utils/parseRedisUrl'
 
 export default defineNitroPlugin((nitroApp) => {
   const config = useRuntimeConfig()
 
-  const questQueue = new Queue('quests', {
-    connection: {
-      host: config.redis.host,
-      port: Number(config.redis.port),
-      password: config.redis.password || undefined,
-    },
-  })
+  const connection = parseRedisUrl(config.redis.url) || {
+    host: config.redis.host,
+    port: Number(config.redis.port),
+    password: config.redis.password || undefined,
+    tls: config.redis.tls ? {} : undefined,
+  }
+
+  const questQueue = new Queue('quests', { connection })
 
   // Register the queue in Nitro's context
   nitroApp.hooks.hook('request', (event) => {
