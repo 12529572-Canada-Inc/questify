949d980aa5d59b7b13e56949777ec83cd9687ceb
Shawn Daichendt <shawn.daichendt@gmail.com>
2025-09-26 22:09:01 -0400

    chore: remove unused authentication middleware and plugin files

--- apps/nuxt/middleware/auth.global.ts
+++
@@ -1,6 +0,0 @@
-export default defineNuxtRouteMiddleware(async (to) => {
-  const { status } = useAuth()
-  if (status.value === 'unauthenticated' && to.path.startsWith('/quests')) {
-    return navigateTo('/auth/login')
-  }
-})
--- apps/nuxt/server/plugins/auth.ts
+++
@@ -1,8 +0,0 @@
-import { authorize } from '../utils/auth'
-
-export default defineNuxtPlugin(() => {
-  const auth = useAuth()
-  if (auth.options.provider?.type === 'credentials') {
-    auth.options.provider.authorize = authorize
-  }
-})
