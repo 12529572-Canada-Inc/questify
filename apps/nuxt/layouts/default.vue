aea238c93c2b05e128f6fd39a4c10942fc7e0b93
Shawn Daichendt <shawn.daichendt@gmail.com>
2025-09-28 19:04:14 -0400

    refactor: update layout structure to use v-main and v-app-bar for better UI organization

--- apps/nuxt/layouts/default.vue
+++ apps/nuxt/layouts/default.vue
@@ -1,8 +1,46 @@
+<script setup lang="ts">
+const { loggedIn } = useUserSession()
+const router = useRouter()
+
+async function logout() {
+  await clearSession()
+  router.push('/auth/login')
+}
+</script>
+
 <template>
-  <v-container
-    fluid
-    class="fill-height d-flex flex-column align-center justify-center"
-  >
+  <v-main>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      <v-toolbar-title>Questify 🎯</v-toolbar-title>
+      <v-spacer />
+
+      <template v-if="loggedIn">
+        <v-btn
+          text
+          @click="logout"
+        >
+          Logout
+        </v-btn>
+      </template>
+      <template v-else>
+        <v-btn
+          text
+          to="/auth/login"
+        >
+          Login
+        </v-btn>
+        <v-btn
+          text
+          to="/auth/signup"
+        >
+          Signup
+        </v-btn>
+      </template>
+    </v-app-bar>
     <slot />
-  </v-container>
+  </v-main>
 </template>
