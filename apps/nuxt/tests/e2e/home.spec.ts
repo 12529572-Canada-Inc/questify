7241aca4cedafbf280b0cda8d97798e77a45ecce
Shawn Daichendt <shawn.daichendt@gmail.com>
2025-10-17 12:05:17 -0400

    test: rename homepage test for clarity and remove commented code

--- apps/nuxt/tests/e2e/home.spec.ts
+++ apps/nuxt/tests/e2e/home.spec.ts
@@ -1,9 +1,16 @@
 import { expect, test } from '@playwright/test'
 
-test('homepage hero is visible', async ({ page }) => {
+test('Home loads correctly', async ({ page }) => {
   await page.goto('/', { waitUntil: 'networkidle' })
 
   await expect(page.getByText('Welcome to Questify')).toBeVisible()
   await expect(page.getByRole('button', { name: 'View Quests' })).toBeVisible()
   await expect(page.getByRole('button', { name: 'Create Quest' })).toBeVisible()
 })
+
+// import { expect, test } from '@nuxt/test-utils/playwright'
+
+// test('test', async ({ page, goto }) => {
+//   await goto('/', { waitUntil: 'hydration' })
+//   await expect(page.getByText('My Test App')).toBeVisible()
+// })
