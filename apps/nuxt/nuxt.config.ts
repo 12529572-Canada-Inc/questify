1641d4794211298d3f214e4c951208dba839dc2e
Shawn Daichendt <shawn.daichendt@gmail.com>
2025-11-17 13:18:05 -0500

    feat: add comment to clarify default image max size of 200MB in nuxt.config.ts

--- apps/nuxt/nuxt.config.ts
+++ apps/nuxt/nuxt.config.ts
@@ -5,7 +5,7 @@ import { loadModelConfig } from '../../packages/shared/src/model-config'
 
 const aiModelConfig = loadModelConfig()
 const aiAssistEnabled = process.env.NUXT_FEATURE_AI_ASSIST === 'true'
-const defaultImageMaxSizeBytes = 200 * 1024 * 1024
+const defaultImageMaxSizeBytes = 200 * 1024 * 1024 // 200MB
 const configuredImageMaxSizeBytes = Number(process.env.NUXT_PUBLIC_IMAGE_MAX_SIZE_BYTES ?? defaultImageMaxSizeBytes)
 const imageMaxSizeBytes = Number.isFinite(configuredImageMaxSizeBytes) && configuredImageMaxSizeBytes > 0
   ? configuredImageMaxSizeBytes
